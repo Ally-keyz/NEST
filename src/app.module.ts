305aa850a20bf898e90df4100d0e3754c1f6cf98
Manzi Alpe <manzialpe@gmail.com>
2025-11-08 15:51:18 +0200

    working with mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { loggerMiddleware } from './logger.middleware';
 import { LogMiddleware } from './log.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal : true}),
+    MongooseModule.forRoot(process.env.MONGO_URL || "mongodb+srv://manzialpe:gloire@cluster0.beqtnkj.mongodb.net/?appName=Cluster0" , {
+      connectionFactory:(connection)=> {
+        connection.on("connected",()=>{
+          console.log("Connected to mongo db ...")
+        })
+
+        connection.on("error" , (err: any)=>{
+          console.log("Failed to connect to mongo db..." , err)
+        })
+       return connection ;
+      }})
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
+ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-    .apply(loggerMiddleware)
-    .forRoutes(AppController);
-
-    consumer
-    .apply(LogMiddleware)
-    .forRoutes('');
+    .apply(AppController)
+    .exclude({path : "cats/*xyz" , method : RequestMethod.ALL})
+    .forRoutes(AppService)
   }
-}
+ }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // use helmet and enable cors to secure request headers
+  app.use(helmet());
+  app.enableCors();
+
+  // enable rate limiting basic
+  app.use(rateLimit.default({
+    windowMs : 60 * 1000 ,
+    max : 100
+  }));
+
+  // use global validation pipeline
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true ,
+      forbidNonWhitelisted : true ,
+      transform : true
+    })
+  )
+  await app.listen(process.env.PORT || 3000);
+  console.log(`App is listening on port : ${process.env.PORT || 3000}`); 
 }
 bootstrap();
